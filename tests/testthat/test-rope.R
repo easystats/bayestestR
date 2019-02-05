a2dddaad0c9469f3bbc811a5dc5100f898f20da8
Dominique Makowski <dom.mak19@gmail.com>
2019-02-05 16:10:37 +0800

    complete testing

--- tests/testthat/test-rope.R
+++ tests/testthat/test-rope.R
@@ -12,4 +12,8 @@ test_that("rope", {
 
   testthat::expect_equal(as.numeric(rope(rnorm_perfect(1000, 1, 1), CI = c(50, 99))$CI_99), 4.94, tolerance = 0.01)
   testthat::expect_equal(equivalence_test(rnorm_perfect(1000, 1, 1), CI = c(50, 99))$CI_99, "undecided")
+
+  set.seed(333)
+  testthat::expect_is(rope(rnorm_perfect(1000, 0, 1)), "rope")
+  testthat::expect_equal(capture.output(print(rope(rnorm_perfect(1000, 0, 1)))), "8.88% in ROPE")
 })
