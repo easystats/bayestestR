8d0803f13a87741b37c1c2e952e412eafc59812a
Daniel <mail@danielluedecke.de>
2019-03-08 14:12:30 +0100

    fix tests

--- tests/testthat/test-rope.R
+++ tests/testthat/test-rope.R
@@ -17,12 +17,14 @@ test_that("rope", {
   testthat::expect_equal(x$ROPE_Percentage[2], 4.94, tolerance = 0.01)
   testthat::expect_equal(x$ROPE_Equivalence[2], "undecided")
 
-  testthat::expect_error(rope(rnorm_perfect(1000, 0, 1), bounds = c(0.0, 0.1, 0.2)))
+  testthat::expect_error(rope(rnorm_perfect(1000, 0, 1), range = c(0.0, 0.1, 0.2)))
 
   set.seed(333)
   testthat::expect_is(rope(rnorm_perfect(1000, 0, 1)), "rope")
-  testthat::expect_equal(capture.output(print(rope(rnorm_perfect(1000, 0, 1)))), "8.88% of the 90% CI is in ROPE [-0.10, 0.10]")
-
-  testthat::expect_error(rope(rnorm_perfect(1000, 0, 1), bounds = c("A", 0.1)))
-  testthat::expect_equal(as.numeric(rope(rnorm_perfect(1000, 0, 1), bounds = c(-0.1, 0.1))), 8.88, tolerance = 0.01)
+  testthat::expect_equal(
+    capture.output(print(rope(rnorm_perfect(1000, 0, 1)))),
+    c("# Proportions of samples inside the ROPE [-0.10, 0.10]", "", " % in ROPE", "      8.88")
+  )
+  testthat::expect_error(rope(rnorm_perfect(1000, 0, 1), range = c("A", 0.1)))
+  testthat::expect_equal(as.numeric(rope(rnorm_perfect(1000, 0, 1), range = c(-0.1, 0.1))), 8.88, tolerance = 0.01)
 })
