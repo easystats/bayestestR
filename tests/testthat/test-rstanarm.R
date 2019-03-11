56079843653e4a02fe925c980e94737ef0d8400a
Dominique Makowski <dom.mak19@gmail.com>
2019-03-11 11:38:03 +0800

    expand rstanarm testing

--- tests/testthat/test-rstanarm.R
+++ tests/testthat/test-rstanarm.R
@@ -7,8 +7,23 @@ test_that("rstanarm", {
   model <- circus::download_model("stanreg_lm_1")
   testthat::expect_equal(rope_range(model)[1], -0.602, tol = 0.1)
 
+  model <- circus::download_model("stanreg_meanfield_lm_1")
+  testthat::expect_equal(rope_range(model)[1], -0.602, tol = 0.1)
+
+  model <- circus::download_model("stanreg_fullrank_lm_1")
+  testthat::expect_equal(rope_range(model)[1], -0.602, tol = 0.1)
+
+  model <- circus::download_model("stanreg_lmerMod_1")
+  testthat::expect_equal(rope_range(model)[1], -0.097, tol = 0.1)
+
   model <- circus::download_model("stanreg_glm_1")
   testthat::expect_equal(rope_range(model)[1], -0.2912962, tol = 0.1)
+
+  model <- circus::download_model("stanreg_merMod_1")
+  testthat::expect_equal(rope_range(model)[1], -0.2912962, tol = 0.1)
+
+  model <- circus::download_model("stanreg_gamm4_1")
+  testthat::expect_equal(rope_range(model)[1], -0.043, tol = 0.1)
 
   testthat::expect_is(hdi(model), "data.frame")
   testthat::expect_is(rope(model), "data.frame")
