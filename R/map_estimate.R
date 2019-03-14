ea167ebab805dd989d5a7fe72d20d0368d00f19a
Daniel <mail@danielluedecke.de>
2019-03-14 18:14:49 +0100

    filter parameters for map_estimate()

--- R/map_estimate.R
+++ R/map_estimate.R
@@ -4,7 +4,9 @@
 #'
 #' @param posterior Vector representing a posterior distribution. Can also be a \code{stanreg} or \code{brmsfit} model.
 #' @param precision Number of points for density estimation. See the \code{n} parameter in \link[=density]{density}.
-#' @param density If TRUE, it will return a data.frame with the x, y coordinates of the MAP, corresponding respectively to the MAP and its density value.
+#' @param density If \code{TRUE}, it will return a data.frame with the x, y coordinates of the MAP, corresponding respectively to the MAP and its density value.
+#'
+#' @inheritParams hdi
 #'
 #' @examples
 #' library(bayestestR)
@@ -12,6 +14,7 @@
 #' posterior <- rnorm(1000)
 #' map_estimate(posterior)
 #' map_estimate(posterior, density = TRUE)
+#'
 #' \dontrun{
 #' library(rstanarm)
 #' model <- rstanarm::stan_glm(mpg ~ wt + cyl, data = mtcars)
@@ -19,47 +22,63 @@
 #'
 #' library(brms)
 #' model <- brms::brm(mpg ~ wt + cyl, data = mtcars)
-#' map_estimate(model)
-#' }
+#' map_estimate(model)}
+#'
 #' @importFrom stats density
 #' @export
-map_estimate <- function(posterior, precision = 2^10, density = FALSE) {
+map_estimate <- function(posterior, ...) {
   UseMethod("map_estimate")
 }
 
 
 #' @export
-map_estimate.numeric <- function(posterior, precision = 2^10, density = FALSE) {
-  d <- density(posterior, n = precision)
+map_estimate.numeric <- function(posterior, precision = 2^10, density = FALSE, ...) {
+  d <- stats::density(posterior, n = precision)
 
   hdp_x <- d$x[which.max(d$y)]
   hdp_y <- max(d$y)
 
   if (density == FALSE) {
-    return(hdp_x)
+    hdp_x
   } else {
-    return(data.frame(
+    data.frame(
       "MAP" = hdp_x,
       "MAP_density" = hdp_y
-    ))
+    )
   }
 }
 
 
 #' @importFrom insight get_parameters
 #' @keywords internal
-.map_estimate_models <- function(posterior, precision = 2^10, density = FALSE) {
-  list <- sapply(insight::get_parameters(posterior), map_estimate, precision = precision, density = TRUE, simplify = FALSE)
+.map_estimate_models <- function(posterior, precision, density) {
+  list <- sapply(posterior, map_estimate, precision = precision, density = TRUE, simplify = FALSE)
   out <- flatten_list(list, name = "Parameter")
+  rownames(out) <- NULL
+
   if (density == TRUE) {
-    return(out)
+    out
   } else {
-    return(out[names(out) != "MAP_density"])
+    out[names(out) != "MAP_density"]
   }
 }
 
+#' @rdname map_estimate
 #' @export
-map_estimate.stanreg <- .map_estimate_models
+map_estimate.stanreg <- function(posterior, precision = 2^10, density = FALSE, effects = c("fixed", "random", "all"), parameters = NULL, ...) {
+  .map_estimate_models(
+    posterior = insight::get_parameters(posterior, effects = effects, parameters = parameters),
+    precision = precision,
+    density = density
+  )
+}
 
+#' @rdname map_estimate
 #' @export
-map_estimate.brmsfit <- .map_estimate_models
+map_estimate.brmsfit <- function(posterior, precision = 2^10, density = FALSE, effects = c("fixed", "random", "all"), component = c("conditional", "zi", "zero_inflated", "all"), parameters = NULL, ...) {
+  .map_estimate_models(
+    posterior = insight::get_parameters(posterior, effects = effects, parameters = parameters),
+    precision = precision,
+    density = density
+  )
+}
