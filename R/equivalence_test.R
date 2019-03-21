3cd1a0e7e9bf93b4bafb141469f9ad34b17001c1
Daniel <mail@danielluedecke.de>
2019-03-21 09:46:41 +0100

    move print.equivalence_test() to separate file

--- R/equivalence_test.R
+++ R/equivalence_test.R
@@ -21,7 +21,7 @@
 #'
 #' @examples
 #' library(bayestestR)
-#' 
+#'
 #' equivalence_test(posterior = rnorm(1000, 0, 0.01), range = c(-0.1, 0.1))
 #' equivalence_test(posterior = rnorm(1000, 0, 1), range = c(-0.1, 0.1))
 #' equivalence_test(posterior = rnorm(1000, 1, 0.01), range = c(-0.1, 0.1))
@@ -31,13 +31,13 @@
 #' model <- rstanarm::stan_glm(mpg ~ wt + cyl, data = mtcars)
 #' equivalence_test(model)
 #' equivalence_test(model, ci = c(.50, 1))
-#' 
+#'
 #' library(brms)
 #' model <- brms::brm(mpg ~ wt + cyl, data = mtcars)
 #' equivalence_test(model)
 #' equivalence_test(model, ci = c(.50, .99))
 #' }
-#' 
+#'
 #' @importFrom insight print_color
 #' @export
 equivalence_test <- function(posterior, ...) {
@@ -70,43 +70,6 @@ equivalence_test.numeric <- function(posterior, range = "default", ci = .95, ver
 
   class(out) <- c("equivalence_test", class(out))
   out
-}
-
-
-#' @export
-print.equivalence_test <- function(x, digits = 2, ...) {
-  insight::print_color("# Test for Practical Equivalence\n\n", "blue")
-  cat(sprintf("  ROPE: [%.*f %.*f]\n\n", digits, x$ROPE_low[1], digits, x$ROPE_high[1]))
-
-  # find the longest HDI-value, so we can align the brackets in the ouput
-  x$HDI_low <- sprintf("%.*f", digits, x$HDI_low)
-  x$HDI_high <- sprintf("%.*f", digits, x$HDI_high)
-
-  maxlen_low <- max(nchar(x$HDI_low))
-  maxlen_high <- max(nchar(x$HDI_high))
-
-  x$ROPE_Percentage <- sprintf("%.*f %%", digits, x$ROPE_Percentage)
-  x$HDI <- sprintf("[%*s %*s]", maxlen_low, x$HDI_low, maxlen_high, x$HDI_high)
-
-  ci <- unique(x$CI)
-  keep.columns <- c("CI", "Parameter", "ROPE_Equivalence", "ROPE_Percentage", "HDI")
-
-  x <- x[, intersect(keep.columns, colnames(x))]
-
-  colnames(x)[which(colnames(x) == "ROPE_Equivalence")] <- "H0"
-  colnames(x)[which(colnames(x) == "ROPE_Percentage")] <- "inside ROPE"
-
-  # clean parameter names
-  if ("Parameter" %in% colnames(x)) {
-    x$Parameter <- gsub("^(b_zi_|b_|bs_|bsp_|bcs_)(.*)", "\\2", x$Parameter)
-  }
-
-  for (i in ci) {
-    xsub <- x[x$CI == i, -which(colnames(x) == "CI"), drop = FALSE]
-    colnames(xsub)[ncol(xsub)] <- sprintf("%i%% HDI", i)
-    print.data.frame(xsub, digits = digits, row.names = FALSE)
-    cat("\n")
-  }
 }
 
 
