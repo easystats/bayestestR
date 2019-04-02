b5931f106eda0f5e80049236768a4eb3b8431dde
Daniel <mail@danielluedecke.de>
2019-04-02 17:42:55 +0200

    Suggestion for #52

--- R/rope_range.R
+++ R/rope_range.R
@@ -1,6 +1,20 @@
 #' @title Find Default Equivalence (ROPE) Region Bounds
 #'
-#' @description This function attempts at finding suitable "default" values for the Region Of Practical Equivalence (ROPE). Kruschke (2018) suggests that such null value could be set, by default, to the \code{-0.1} to \code{0.1} range of a standardized parameter (negligible effect size according to Cohen, 1988), which can be generalised for linear models to \eqn{[-0.1*SD_{y}, 0.1*SD_{y}]}.
+#' @description This function attempts at finding suitable "default" values
+#'   for the Region Of Practical Equivalence (ROPE). Kruschke (2018) suggests
+#'   that such null value could be set, by default, to a range from \code{-0.1} to
+#'   \code{0.1} times of a standardized parameter (negligible effect size
+#'   according to Cohen, 1988), which can be generalised for linear models
+#'   to \ifelse{html}{\out{-0.1 * SD<sub>y</sub>, 0.1 * SD<sub>y</sub>}}{\eqn{[-0.1*SD_{y}, 0.1*SD_{y}]}}.
+#'   \cr \cr
+#'   For models with binary outcome, there is no direct way to specify the
+#'   effect size that defines the ROPE limits. Two examples from Kruschke suggest
+#'   that a negligible change is about .05 on the logit-scale. In these cases,
+#'   it is recommended to specify the rope argument, however, if not specified,
+#'   the ROPE limits are caluclated as suggested by Kruschke, i.e. the effect
+#'   size is the probability of "success" for the outcome, divided by \code{pi}.
+#'   \cr \cr
+#'   For all other models, \code{-0.1, 0.1} is used to determine the ROPE limits.
 #'
 #' @inheritParams rope
 #'
