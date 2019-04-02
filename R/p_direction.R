0bd44d6f9e7df7bee9e729f3a1d3cb34d9fafecf
Daniel <mail@danielluedecke.de>
2019-04-02 15:40:26 +0200

    typo

--- R/p_direction.R
+++ R/p_direction.R
@@ -5,11 +5,11 @@
 #' @param posterior Vector representing a posterior distribution. Can also be a \code{stanreg} or \code{brmsfit} model.
 #' @inheritParams hdi
 #'
-#' @details \strong{Relationship with the p-value}: In most cases, it seems that the \emph{p}d corresponds to the frequentist one-sided \emph{p}-value through the formula `p-value = (1-pd/100)`. Thus, a `pd` of `95\%`, `97.5\%` `99.5\%` and `99.95\%` corresponds approximately to a two-sided \emph{p}-value of respectively `.1`, `.05`, `.01` and `.001`. See the \href{https://easystats.github.io/bayestestR/articles/4_Guidelines.html}{\emph{reporting guidelines}}.
+#' @details \strong{Relationship with the p-value}: In most cases, it seems that the \emph{p}d corresponds to the frequentist one-sided \emph{p}-value through the formula \code{p-value = (1-pd/100)}. Thus, a \code{pd} of \code{95\%}, \code{97.5\%}, \code{99.5\%} and \code{99.95\%} corresponds approximately to a two-sided \emph{p}-value of respectively \code{.1}, \code{.05}, \code{.01} and \code{.001}. See the \href{https://easystats.github.io/bayestestR/articles/4_Guidelines.html}{\emph{reporting guidelines}}.
 #'
 #' @examples
 #' library(bayestestR)
-#' 
+#'
 #' # Simulate a posterior distribution of mean 1 and SD 1
 #' posterior <- rnorm(1000, mean = 1, sd = 1)
 #' p_direction(posterior)
@@ -17,12 +17,12 @@
 #' library(rstanarm)
 #' model <- rstanarm::stan_glm(mpg ~ wt + cyl, data = mtcars)
 #' p_direction(model)
-#' 
+#'
 #' library(brms)
 #' model <- brms::brm(mpg ~ wt + cyl, data = mtcars)
 #' p_direction(model)
 #' }
-#' 
+#'
 #' @export
 p_direction <- function(posterior, ...) {
   UseMethod("p_direction")
