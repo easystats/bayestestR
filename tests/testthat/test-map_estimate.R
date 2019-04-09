941aa6f4afddd98a1666e9ea938b49aab7437e47
Daniel Lüdecke <mail@danielluedecke.de>
2019-04-09 09:37:34 +0200

    fix test

--- tests/testthat/test-map_estimate.R
+++ tests/testthat/test-map_estimate.R
@@ -1,5 +1,9 @@
 context("map_estimate")
 
 test_that("map_estimate", {
-  testthat::expect_equal(map_estimate(rnorm_perfect(1000)), 0, tolerance = 0.01)
+  testthat::expect_equal(
+    map_estimate(rnorm_perfect(1000)),
+    structure(-0.00368006135440124, MAP_density = 0.389791625903652, class = c("numeric", "MAP")),
+    tolerance = 0.01
+  )
 })
