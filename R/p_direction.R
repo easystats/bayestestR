6ebcb310aa92420ee04e26a1250e0478c9b55d22
Dominique Makowski <dom.mak19@gmail.com>
2019-04-13 10:21:12 +0800

    fixes and updated README

--- R/p_direction.R
+++ R/p_direction.R
@@ -31,10 +31,14 @@ p_direction <- function(x, ...) {
 
 #' @export
 print.p_direction <- function(x, ...) {
-  if("data.frame" %in% class(x)){
-    cat(paste0(paste0("  - ", x$Parameter, sprintf(": pd = %.2f%%", x$pd)), collapse = "\n"))
+  if("data_plot" %in% class(x)){
+    print(as.data.frame(x))
   } else{
-    cat(sprintf("pd = %.2f%%", x))
+    if("data.frame" %in% class(x)){
+      cat(paste0(paste0("  - ", x$Parameter, sprintf(": pd = %.2f%%", x$pd)), collapse = "\n"))
+    } else{
+      cat(sprintf("pd = %.2f%%", x))
+    }
   }
 }
 
