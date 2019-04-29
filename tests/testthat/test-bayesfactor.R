e292604a3295333656767342247f1fb34e2d8113
Dominique Makowski <dom.mak19@gmail.com>
2019-04-29 22:32:28 +0800

    fix bayesfactor tests

--- tests/testthat/test-bayesfactor.R
+++ tests/testthat/test-bayesfactor.R
@@ -5,12 +5,12 @@ test_that("bayesfactor_savagedickey", {
   Xprior <- rnorm(1000)
   Xposterior <- rnorm(1000,0.7,0.2)
 
-  bayesfactor_savagedickey <- bayestestR::bayesfactor_savagedickey(Xposterior, prior = Xprior, h0 = 0, direction = 0)
+  bayesfactor_savagedickey <- bayestestR::bayesfactor_savagedickey(Xposterior, prior = Xprior, hypothesis = 0, direction = 0)
   testthat::expect_equal(as.numeric(bayesfactor_savagedickey), 39.6, tolerance = 0.1)
 
-  bayesfactor_savagedickey <- bayestestR::bayesfactor_savagedickey(Xposterior, prior = Xprior, h0 = 0, direction = 1)
+  bayesfactor_savagedickey <- bayestestR::bayesfactor_savagedickey(Xposterior, prior = Xprior, hypothesis = 0, direction = 1)
   testthat::expect_equal(as.numeric(bayesfactor_savagedickey), 76.8, tolerance = 0.1)
 
-  bayesfactor_savagedickey <- bayestestR::bayesfactor_savagedickey(Xposterior, prior = Xprior, h0 = 1, direction = 0)
+  bayesfactor_savagedickey <- bayestestR::bayesfactor_savagedickey(Xposterior, prior = Xprior, hypothesis = 1, direction = 0)
   testthat::expect_equal(as.numeric(bayesfactor_savagedickey), 0.4, tolerance = 0.1)
 })
