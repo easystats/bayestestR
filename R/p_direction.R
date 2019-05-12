9ae117af3e250fc21f875373ec74c7586a1055a4
Daniel <mail@danielluedecke.de>
2019-05-12 18:11:18 +0200

    rename var to avoid confusion

--- R/p_direction.R
+++ R/p_direction.R
@@ -62,7 +62,7 @@ pd <- p_direction
 #' @export
 p_direction.numeric <- function(x, method = "direct", ...) {
   if (method == "direct") {
-    p_direction <- 100 * max(
+    pdir <- 100 * max(
       c(
         length(x[x > 0]) / length(x), # pd positive
         length(x[x < 0]) / length(x) # pd negative
@@ -75,13 +75,16 @@ p_direction.numeric <- function(x, method = "direct", ...) {
     } else {
       dens <- dens[dens$x < 0, ]
     }
-    p_direction <- area_under_curve(dens$x, dens$y, method = "spline") * 100
-    if (p_direction >= 100) p_direction <- 100
+    pdir <- area_under_curve(dens$x, dens$y, method = "spline") * 100
+    if (pdir >= 100) pdir <- 100
   }
 
-  attr(p_direction, "method") <- method
-  class(p_direction) <- c("p_direction", class(p_direction))
-  p_direction
+  attr(pdir, "method") <- method
+  attr(pdir, "data") <- x
+
+  class(pdir) <- c("p_direction", class(pdir))
+
+  pdir
 }
 
 
