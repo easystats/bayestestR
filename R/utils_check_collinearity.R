c7c3f5077be30a2e357449a67a00282db35eb34e
Dominique Makowski <dom.mak19@gmail.com>
2019-05-14 09:05:36 +0800

    more details in collinearity warning

--- R/utils_check_collinearity.R
+++ R/utils_check_collinearity.R
@@ -1,7 +1,7 @@
 #' @importFrom stats cor cor.test
 #' @importFrom insight find_parameters
 #' @keywords internal
-.check_multicollinearity <- function(model, method = "equivalence_test") {
+.check_multicollinearity <- function(model, method = "equivalence_test", threshold = 0.5, ...) {
   valid_parameters <- insight::find_parameters(model, parameters = "^(?!(r_|sd_|prior_|cor_|b\\[))", flatten = TRUE)
   dat <- as.data.frame(model)[, valid_parameters]
   dat <- dat[, -1, drop = FALSE]
@@ -16,10 +16,28 @@
       pvalue = apply(parameter, 1, function(r) stats::cor.test(dat[[r[1]]], dat[[r[2]]])$p.value)
     )
 
+    # Filter
     results <- results[results$pvalue < 0.05 & results$Var1 != results$Var2, ]
 
-    if (nrow(results) > 0 && any(results$corr >= 0.5)) {
-      warning("Possible multicollinearity, leading to inappropriate results. See 'Details' in '?", method, "'.", call. = FALSE)
+    if (nrow(results) > 0){
+
+      # Remove duplicates
+      results$where <- paste0(results$Var1, " and ", results$Var2)
+      results$where2 <- paste0(results$Var2, " and ", results$Var1)
+      to_remove <- c()
+      for(i in 1:nrow(results)){
+        if(results$where2[i] %in% results$where[1:i]){
+          to_remove <- c(to_remove, i)
+        }
+      }
+      results <- results[-to_remove, ]
+
+      # Filter by threshold
+      results <- results[results$corr > threshold, ]
+      if(nrow(results) > 0){
+        where <- paste0("between ", paste0(paste0(results$where, " (r = ", round(results$corr, 2), ")"), collapse = ", "), "")
+        warning("Possible multicollinearity ", where, ". This might lead to inappropriate results. See 'Details' in '?", method, "'.", call. = FALSE)
+      }
     }
   }
 }
