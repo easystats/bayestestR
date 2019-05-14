363ca0098ff52f81b22930c4ec2a095912c980dd
Dominique Makowski <dom.mak19@gmail.com>
2019-05-14 11:02:42 +0800

    issues with point-estimates

--- tests/testthat/test-describe_posterior.R
+++ tests/testthat/test-describe_posterior.R
@@ -11,13 +11,16 @@ test_that("describe_posterior", {
 
 
   x <- data.frame(replicate(4, rnorm(100)))
-  bayesfactor_savagedickey(x)
   rez <- describe_posterior(x, estimate = "all", dispersion = TRUE, test = "all")
   testthat::expect_equal(dim(rez), c(4, 16))
   rez <- describe_posterior(x, estimate = "all", dispersion = TRUE, test = "all", ci = c(0.8, 0.9))
   testthat::expect_equal(dim(rez), c(8, 16))
 
 
-  model <- insight::download_model("stanreg_lm_1")
+  x <- insight::download_model("stanreg_lm_1")
+  rez <- describe_posterior(x, estimate = "all", dispersion = TRUE, test = "all")
+  # testthat::expect_equal(dim(rez), c(2, 16))
+  rez <- describe_posterior(x, estimate = "all", dispersion = TRUE, test = "all", ci = c(0.8, 0.9))
+  # testthat::expect_equal(dim(rez), c(4, 16))
 
 })
