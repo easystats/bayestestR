c45a946263f24bf0592bbca214d5a65b0482e827
Dominique Makowski <dom.mak19@gmail.com>
2019-05-22 21:26:03 +0800

    Update effective_sample.R

--- R/effective_sample.R
+++ R/effective_sample.R
@@ -10,7 +10,10 @@
 #'
 #' @details \strong{Effective Sample (ESS)} should be as large as possible, altough for most applications, an effective sample size greater than 1,000 is sufficient for stable estimates (Bürkner, 2017). The ESS corresponds to the number of independent samples with the same estimation power as the N autocorrelated samples. It is is a measure of \dQuote{how much independent information there is in autocorrelated chains} (\emph{Kruschke 2015, p182-3}).
 #'
-#' @references Kruschke, J. (2014). Doing Bayesian data analysis: A tutorial with R, JAGS, and Stan. Academic Press.
+#' @references \itemize{
+#'   \item Kruschke, J. (2014). Doing Bayesian data analysis: A tutorial with R, JAGS, and Stan. Academic Press.
+#'   \item Bürkner, P. C. (2017). brms: An R package for Bayesian multilevel models using Stan. Journal of Statistical Software, 80(1), 1-28
+#' }
 #'
 #' @examples
 #' \dontrun{
