c4f347c5c99606679937c1c5707b9964b4701487
Daniel Lüdecke <mail@danielluedecke.de>
2019-05-25 19:16:12 +0200

    tweak docs and print method

--- R/print.p_direction.R
+++ R/print.p_direction.R
@@ -3,10 +3,12 @@ print.p_direction <- function(x, ...) {
   if ("data_plot" %in% class(x)) {
     print(as.data.frame(x))
   } else {
-    insight::print_color("# Probability of Direction (pd):\n\n", "blue")
+    insight::print_color("# Probability of Direction (pd)\n\n", "blue")
 
     if ("data.frame" %in% class(x)) {
-      cat(paste0(paste0("  - ", x$Parameter, sprintf(": pd = %.2f%%", x$pd)), collapse = "\n"))
+      pars <- format(x$Parameter)
+      pd <- format(sprintf("%.2f%%", x$pd), justify = "right")
+      cat(paste0("  ", pars, ": ", pd, collapse = "\n"))
     } else {
       cat(sprintf("  - pd = %.2f%%", x))
     }
