88e1059aea8be29522e21e00d21b14d2dd97c48f
mattansb <35330040+mattansb@users.noreply.github.com>
2019-06-08 17:29:32 +0300

    skip_on_travis for all emmGrid

--- tests/testthat/test-emmGrid.R
+++ tests/testthat/test-emmGrid.R
@@ -11,6 +11,7 @@ all_ <- rbind(em_, c_)
 all_summ <- summary(all_)
 
 test_that("emmGrid ci", {
+  testthat::skip_on_travis()
   xci <- ci(all_, ci = 0.9)
   testthat::expect_equal(xci$CI_low, c(-0.236749774206338, 1.23103419307697, -2.99025704276072), tolerance = 1e-4)
   testthat::expect_equal(xci$CI_high, c(1.83, 3.35, -0.02), tolerance = 0.2)
@@ -24,12 +25,14 @@ test_that("emmGrid equivalence_test", {
 })
 
 test_that("emmGrid estimate_density", {
+  testthat::skip_on_travis()
   xestden <- estimate_density(c_, method = "logspline", precision = 5)
   testthat::expect_equal(xestden$x, c(-4.67, -2.91, -1.16, 0.60, 2.35), tolerance = 0.2)
   testthat::expect_equal(log(xestden$y), c(-6.18, -2.12, -0.86, -3.62,-7.90), tolerance = 0.2)
 })
 
 test_that("emmGrid hdi", {
+  testthat::skip_on_travis()
   xhdi <- hdi(all_, ci = 0.95)
   testthat::expect_equal(xhdi$CI_low, c(-0.41, 0.99, -3.23), tolerance = 0.2)
   testthat::expect_equal(xhdi$CI_high, c(2.06, 3.56, 0.28), tolerance = 0.2)
@@ -38,21 +41,25 @@ test_that("emmGrid hdi", {
 })
 
 test_that("emmGrid p_direction", {
+  testthat::skip_on_travis()
   xpd <- p_direction(all_, method = "direct")
   testthat::expect_equal(xpd$pd, c(90.25, 99.9, 95.2), tolerance = 0.1)
 })
 
 test_that("emmGrid p_map", {
+  testthat::skip_on_travis()
   xpmap <- p_map(all_, precision = 2^9)
   testthat::expect_equal(xpmap$p_MAP, c(0.42, 0, 0.26), tolerance = 0.1)
 })
 
 test_that("emmGrid p_rope", {
+  testthat::skip_on_travis()
   xprope <- p_rope(all_, range = c(-0.1, 0.1), precision = 0.5)
   testthat::expect_equal(xprope$p_ROPE, c(69.5, 100, 87), tolerance = 0.1)
 })
 
 test_that("emmGrid point_estimate", {
+  testthat::skip_on_travis()
   xpest <- point_estimate(all_, centrality = "median", dispersion = TRUE)
   testthat::expect_equal(xpest$Median, c(0.78, 2.29, -1.52), tolerance = 0.1)
   testthat::expect_equal(xpest$MAD, c(0.60, 0.61, 0.88), tolerance = 0.1)
