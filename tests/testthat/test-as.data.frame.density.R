1d94a800e533129abb1a3897c63b3319bb8d87d7
Daniel <mail@danielluedecke.de>
2019-06-20 11:36:05 +0200

    fix test warning

--- tests/testthat/test-as.data.frame.density.R
+++ tests/testthat/test-as.data.frame.density.R
@@ -1,5 +1,5 @@
 context("as.data.frame.density")
 
 test_that("as.data.frame.density", {
-  testthat::expect_is(as.data.frame(density(rnorm_perfect(1000))), "data.frame")
+  testthat::expect_is(as.data.frame(density(distribution_normal(1000))), "data.frame")
 })
