35c24526a9e22f5feb700b0585a2173529302d81
Daniel <mail@danielluedecke.de>
2019-06-26 15:58:47 +0200

    tibble and list-fix for .is_empty_object

--- R/utils.R
+++ R/utils.R
@@ -21,6 +21,14 @@
 # is string empty?
 #' @keywords internal
 .is_empty_object <- function(x) {
+  if (is.list(x)) {
+    x <- tryCatch(
+      {.compact_list(x)},
+      error = function(x) { x }
+    )
+  }
+  # this is an ugly fix because of ugly tibbles
+  if (inherits(x, c("tbl_df", "tbl"))) x <- as.data.frame(x)
   x <- suppressWarnings(x[!is.na(x)])
   length(x) == 0 || is.null(x)
 }
