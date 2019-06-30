f61276fbfd63c8030557b3cc31268c7ce3ddbb21
mattansb <35330040+mattansb@users.noreply.github.com>
2019-06-30 07:23:24 +0300

    bf_res in master function
    #177

--- R/bayesfactor.R
+++ R/bayesfactor.R
@@ -8,7 +8,8 @@
 #' For a complete overview of these functions, read the \href{https://easystats.github.io/bayestestR/articles/bayes_factors.html}{Bayes factor vignette}.
 #'
 #' @param ... A numeric vector, model object(s), or the output from \code{bayesfactor_models}.
-#' @inheritParams bayesfactor_savagedickey
+#' @inheritParams bayesfactor_parameters
+#' @inheritParams bayesfactor_restricted
 #' @inheritParams bayesfactor_models
 #' @inheritParams bayesfactor_inclusion
 #'
@@ -44,14 +45,14 @@
 #' @export
 bayesfactor <-
   function(...,
-             prior = NULL,
-             direction = "two-sided",
-             hypothesis = 0,
-             effects = c("fixed", "random", "all"),
-             verbose = TRUE,
-             denominator = 1,
-             match_models = FALSE,
-             prior_odds = NULL) {
+           prior = NULL,
+           direction = "two-sided",
+           null = 0,
+           effects = c("fixed", "random", "all"),
+           verbose = TRUE,
+           denominator = 1,
+           match_models = FALSE,
+           prior_odds = NULL) {
     mods <- list(...)
 
     if (length(mods) > 1) {
@@ -64,10 +65,18 @@ bayesfactor <-
       } else {
         bayesfactor_models(...)
       }
+    } else if ("hypothesis" %in% names(list(...))) {
+      bayesfactor_restricted(...,
+                             prior = prior,
+                             verbose = verbose,
+                             effects = effects)
     } else {
-      bayesfactor_savagedickey(...,
-        prior = prior, direction = direction,
-        hypothesis = hypothesis, effects = effects,
+      bayesfactor_parameters(
+        ...,
+        prior = prior,
+        direction = direction,
+        null = null,
+        effects = effects,
         verbose = verbose
       )
     }
