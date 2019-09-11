face2d5693b1a87a8b6270f11f13de2bcb917d6a
Daniel Lüdecke <mail@danielluedecke.de>
2019-09-11 15:10:24 +0200

    fix test

--- tests/testthat/test-map_estimate.R
+++ tests/testthat/test-map_estimate.R
@@ -22,10 +22,14 @@ if (require("insight")) {
   })
 
   m <- insight::download_model("brms_zi_3")
-  cn <- colnames(as.data.frame(m))
-  cn <- cn[!grepl("^(sd_|lp_)", cn)]
 
   test_that("map_estimate", {
-    testthat::expect_equal(map_estimate(m, effects = "all", component = "all")$Parameter, cn)
+    testthat::expect_equal(
+      map_estimate(m, effects = "all", component = "all")$Parameter,
+      c("b_Intercept", "b_child", "b_camper", "r_persons.1.Intercept.",
+        "r_persons.2.Intercept.", "r_persons.3.Intercept.", "r_persons.4.Intercept.",
+        "b_zi_Intercept", "b_zi_child", "b_zi_camper", "r_persons__zi.1.Intercept.",
+        "r_persons__zi.2.Intercept.", "r_persons__zi.3.Intercept.", "r_persons__zi.4.Intercept.")
+    )
   })
 }
