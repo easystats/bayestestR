8a02d527a4337956acea4af9e4a51d8b223d65fc
Daniel Lüdecke <mail@danielluedecke.de>
2019-09-11 14:55:40 +0200

    fix test

--- tests/testthat/test-map_estimate.R
+++ tests/testthat/test-map_estimate.R
@@ -17,7 +17,8 @@ if (require("insight")) {
 
   test_that("map_estimate", {
     testthat::expect_equal(
-      rope(m, effects = "all")$ROPE_Percentage,
+      # fix range to -.1/.1, to compare to data frame method
+      rope(m, range = c(-.1, .1), effects = "all")$ROPE_Percentage,
       rope(p)$ROPE_Percentage,
       tolerance = 1e-3
     )
@@ -28,7 +29,7 @@ if (require("insight")) {
 
   test_that("map_estimate", {
     testthat::expect_equal(
-      rope(m, effects = "all", component = "all")$ROPE_Percentage,
+      rope(m, range = c(-.1, .1), effects = "all", component = "all")$ROPE_Percentage,
       rope(p)$ROPE_Percentage,
       tolerance = 1e-3
     )
