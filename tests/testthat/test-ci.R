42c1b8d48894f6ac7fc41d77c475a79c62bd3bfb
mattansb <35330040+mattansb@users.noreply.github.com>
2019-09-13 18:25:13 +0300

    distribution_normal in tests

--- tests/testthat/test-ci.R
+++ tests/testthat/test-ci.R
@@ -1,15 +1,15 @@
 context("ci")
 
 test_that("ci", {
-  testthat::expect_equal(ci(rnorm_perfect(1000), ci = .90)$CI_low[1], -1.6361, tolerance = 0.02)
-  testthat::expect_equal(nrow(ci(rnorm_perfect(1000), ci = c(.80, .90, .95))), 3, tolerance = 0.01)
-  testthat::expect_equal(ci(rnorm_perfect(1000), ci = 1)$CI_low[1], -3.09, tolerance = 0.02)
-  # testthat::expect_equal(length(capture.output(print(ci(rnorm_perfect(1000))))))
-  # testthat::expect_equal(length(capture.output(print(ci(rnorm_perfect(1000), ci = c(.80, .90))))))
+  testthat::expect_equal(ci(distribution_normal(1000), ci = .90)$CI_low[1], -1.6361, tolerance = 0.02)
+  testthat::expect_equal(nrow(ci(distribution_normal(1000), ci = c(.80, .90, .95))), 3, tolerance = 0.01)
+  testthat::expect_equal(ci(distribution_normal(1000), ci = 1)$CI_low[1], -3.09, tolerance = 0.02)
+  # testthat::expect_equal(length(capture.output(print(ci(distribution_normal(1000))))))
+  # testthat::expect_equal(length(capture.output(print(ci(distribution_normal(1000), ci = c(.80, .90))))))
 
   testthat::expect_warning(ci(c(2, 3, NA)))
   testthat::expect_warning(ci(c(2, 3)))
-  testthat::expect_warning(ci(rnorm_perfect(1000), ci = 950))
+  testthat::expect_warning(ci(distribution_normal(1000), ci = 950))
 
   x <- data.frame(replicate(4, rnorm(100)))
   x <- ci(x, ci = c(0.68, 0.89, 0.95))
--- tests/testthat/test-p_map.R
+++ tests/testthat/test-p_map.R
@@ -1,10 +1,10 @@
 context("p_map")
 
 test_that("p_map", {
-  testthat::expect_equal(as.numeric(p_map(rnorm_perfect(1000))), 1, tolerance = 0.01)
-  testthat::expect_equal(as.numeric(p_map(rnorm_perfect(1000, 1, 1))), 0.62, tolerance = 0.01)
-  testthat::expect_equal(as.numeric(p_map(rnorm_perfect(1000, 2, 1))), 0.15, tolerance = 0.01)
-  testthat::expect_equal(as.numeric(p_map(rnorm_perfect(1000, 3, 0.01))), 0, tolerance = 0.01)
+  testthat::expect_equal(as.numeric(p_map(distribution_normal(1000))), 1, tolerance = 0.01)
+  testthat::expect_equal(as.numeric(p_map(distribution_normal(1000, 1, 1))), 0.62, tolerance = 0.01)
+  testthat::expect_equal(as.numeric(p_map(distribution_normal(1000, 2, 1))), 0.15, tolerance = 0.01)
+  testthat::expect_equal(as.numeric(p_map(distribution_normal(1000, 3, 0.01))), 0, tolerance = 0.01)
 })
 
 
