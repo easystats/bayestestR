00d5a0fdcd04fd66a8fd802c4aac80fde3b3920f
Dominique Makowski <dom.mak19@gmail.com>
2019-10-06 14:32:50 +0800

    remvoe rope_probalistic file

--- R/rope.R
+++ R/rope.R
@@ -433,3 +433,20 @@ rope.sim <- function(x, range = "default", ci = .89, ci_method = "HDI", paramete
     HDI_area = HDI_area
   )
 }
+
+
+
+
+
+
+#' @keywords internal
+# .rope_probabilistic <- function(x, range = c(-0.1/3, 0.1/3)) {
+#
+#   location <- mean(c(range[2], range[1]))
+#   scale <- range[2] - location
+#   rope <- distribution_normal(length(x), location, scale)
+#
+#
+#   rope_overlap <- overlap(posterior, norm) * 100
+#
+# }
