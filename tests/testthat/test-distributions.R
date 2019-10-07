73fd132647bf63e645be4d45bea86952001a8f2e
Dominique Makowski <dom.mak19@gmail.com>
2019-10-07 19:36:47 +0800

    tests for distributions

--- tests/testthat/test-distributions.R
+++ tests/testthat/test-distributions.R
@@ -1,6 +1,32 @@
-context("rnorm")
+context("distributions")
 
-test_that("rnorm", {
-  x <- bayestestR::distribution_normal(10, 0, 1)
-  testthat::expect_equal(mean(x), 0, tolerance = 0.02)
+test_that("distributions", {
+
+  testthat::expect_equal(mean(bayestestR::distribution_normal(10)), 0, tolerance = 0.01)
+  testthat::expect_equal(length(bayestestR::distribution_normal(10, random = TRUE)), 10, tolerance = 0.01)
+
+  testthat::expect_equal(mean(bayestestR::distribution_beta(10, 1, 1)), 0.5, tolerance = 0.01)
+  testthat::expect_equal(length(bayestestR::distribution_normal(10, 1, 1, random = TRUE)), 10, tolerance = 0.01)
+
+  testthat::expect_equal(mean(bayestestR::distribution_binomial(10, 0, 0.5)), 0, tolerance = 0.01)
+  testthat::expect_equal(length(bayestestR::distribution_binomial(10, 0, 0.5, random = TRUE)), 10, tolerance = 0.01)
+
+  testthat::expect_equal(mean(bayestestR::distribution_cauchy(10)), 0, tolerance = 0.01)
+  testthat::expect_equal(length(bayestestR::distribution_cauchy(10, random = TRUE)), 10, tolerance = 0.01)
+
+  testthat::expect_equal(mean(bayestestR::distribution_chisquared(10, 1)), 0.778, tolerance = 0.01)
+  testthat::expect_equal(length(bayestestR::distribution_chisquared(10, 1, random = TRUE)), 10, tolerance = 0.01)
+
+  testthat::expect_equal(mean(bayestestR::distribution_gamma(10, 1)), 0.874, tolerance = 0.01)
+  testthat::expect_equal(length(bayestestR::distribution_gamma(10, 1, random = TRUE)), 10, tolerance = 0.01)
+
+  testthat::expect_equal(mean(bayestestR::distribution_poisson(10)), 0.8, tolerance = 0.01)
+  testthat::expect_equal(length(bayestestR::distribution_poisson(10, random = TRUE)), 10, tolerance = 0.01)
+
+  testthat::expect_equal(mean(bayestestR::distribution_student(10, 1)), 0, tolerance = 0.01)
+  testthat::expect_equal(length(bayestestR::distribution_student(10, 1, random = TRUE)), 10, tolerance = 0.01)
+
+  testthat::expect_equal(mean(bayestestR::distribution_uniform(10)), 0.5, tolerance = 0.01)
+  testthat::expect_equal(length(bayestestR::distribution_uniform(10, random = TRUE)), 10, tolerance = 0.01)
+
 })
