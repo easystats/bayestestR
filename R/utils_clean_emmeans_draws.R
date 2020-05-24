6ec4f1fc55e4f60c6bc168ac3fc093f5d79639cd
DominiqueMakowski <dom.mak19@gmail.com>
2020-05-24 12:20:48 +0800

    propagate link function from emmeans
    https://github.com/easystats/modelbased/pull/64

--- R/utils_clean_emmeans_draws.R
+++ R/utils_clean_emmeans_draws.R
@@ -4,10 +4,16 @@
     stop("Package 'emmeans' required for this function to work. Please install it by running `install.packages('emmeans')`.")
   }
 
+  if(!is.null(attributes(x)$misc$predict.type)){
+    x <- emmeans::regrid(x, transform=attributes(x)$misc$predict.type, ...)
+  }
+
   draws <- emmeans::as.mcmc.emmGrid(
     x,
     names = FALSE,
-    sep.chains = FALSE
+    sep.chains = FALSE,
+    NE.include = TRUE,
+    ...
   )
   data.frame(draws, check.names = FALSE)
 }
