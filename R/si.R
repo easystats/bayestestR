a32ba55c7a7180a52c8d192b96516a0d44304054
mattansb <35330040+mattansb@users.noreply.github.com>
2020-05-29 11:25:49 +0300

    #313

--- R/si.R
+++ R/si.R
@@ -246,8 +246,8 @@ si.data.frame <- function(posterior, prior = NULL, BF = 1, verbose = TRUE, ...){
 
   x_axis <- seq(x_range[1], x_range[2], length.out = precision)
 
-  f_prior <- logspline::logspline(prior, ...)
-  f_posterior <- logspline::logspline(posterior, ...)
+  f_prior <- .logspline(prior, ...)
+  f_posterior <- .logspline(posterior, ...)
   d_prior <- logspline::dlogspline(x_axis, f_prior)
   d_posterior <- logspline::dlogspline(x_axis, f_posterior)
 
