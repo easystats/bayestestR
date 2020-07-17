a11bd762ab4fb2e84afaf7b15749fa3c043316e1
mattansb <35330040+mattansb@users.noreply.github.com>
2020-07-17 14:24:49 +0300

    Update test-bayesfactor_restricted.R
    #327

--- tests/testthat/test-bayesfactor_restricted.R
+++ tests/testthat/test-bayesfactor_restricted.R
@@ -1,18 +1,20 @@
 if (require("rstanarm") &&
     require("BayesFactor") &&
     require("testthat")) {
+
+# bayesfactor_restricted data.frame ---------------------------------------
+
   test_that("bayesfactor_restricted df", {
-    set.seed(444)
     prior <- data.frame(
-      X = rnorm(100),
-      X1 = rnorm(100),
-      X3 = rnorm(100)
+      X = distribution_normal(100),
+      X1 = c(distribution_normal(50),distribution_normal(50)),
+      X3 = c(distribution_normal(80),distribution_normal(20))
     )
 
     posterior <- data.frame(
-      X = rnorm(100, .4, .2),
-      X1 = rnorm(100, -.2, .2),
-      X3 = rnorm(100, .2)
+      X = distribution_normal(100, .4, .2),
+      X1 = distribution_normal(100, -.2, .2),
+      X3 = distribution_normal(100, .2)
     )
 
     hyps <- c(
@@ -20,19 +22,22 @@ if (require("rstanarm") &&
       "X > X1"
     )
 
-    bfr <- bayestestR::bayesfactor_restricted(posterior, hypothesis = hyps, prior = prior)
+    bfr <- bayesfactor_restricted(posterior, hypothesis = hyps, prior = prior)
 
-    testthat::expect_equal(bfr$Prior_prob, c(1 / 6, 1 / 2), tolerance = 0.1)
-    testthat::expect_equal(bfr$Posterior_prob, c(0.32, 0.99), tolerance = 0.1)
-    testthat::expect_equal(log(bfr$BF), c(0.52, 0.76), tolerance = 0.1)
+    testthat::expect_equal(bfr$Prior_prob, c(0.2, 0.5), tolerance = 0.1)
+    testthat::expect_equal(bfr$Posterior_prob, c(0.31, 1), tolerance = 0.1)
+    testthat::expect_equal(log(bfr$BF), c(0.43, 0.69), tolerance = 0.1)
     testthat::expect_equal(bfr$BF, bfr$Posterior_prob / bfr$Prior_prob, tolerance = 0.1)
 
-    testthat::expect_error(bayestestR::bayesfactor_restricted(posterior, prior, hypothesis = "Y < 0"))
+    testthat::expect_error(bayesfactor_restricted(posterior, prior, hypothesis = "Y < 0"))
   })
 
 
+# bayesfactor_restricted RSTANARM -----------------------------------------
+
+
   test_that("bayesfactor_restricted RSTANARM", {
-    set.seed(444)
+    testthat::skip_on_cran()
     library(rstanarm)
     fit_stan <- stan_glm(mpg ~ wt + cyl + am, data = mtcars, refresh = 0)
 
@@ -41,10 +46,14 @@ if (require("rstanarm") &&
       "cyl < 0",
       "wt - cyl > 0"
     )
-    bfr <- bayestestR::bayesfactor_restricted(fit_stan, hypothesis = hyps)
 
-    testthat::expect_equal(length(bfr$Prior_prob), 3)
-    testthat::expect_equal(length(bfr$Posterior_prob), 3)
-    testthat::expect_equal(length(log(bfr$BF)), 3)
+    set.seed(444)
+    fit_p <- update(fit_stan, prior_PD = TRUE)
+    bfr1 <- bayesfactor_restricted(fit_stan, prior = fit_p, hypothesis = hyps)
+
+    set.seed(444)
+    bfr2 <- bayesfactor_restricted(fit_stan, hypothesis = hyps)
+
+    testthat::expect_equal(bfr1, bfr2)
   })
 }
