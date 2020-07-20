48e6ef4a59e1156a8ac33658d9a82ad0be8a8fc3
Daniel <mail@danielluedecke.de>
2020-07-20 15:37:45 +0200

    remove invalid data

--- R/utils.R
+++ R/utils.R
@@ -144,6 +144,7 @@
   merge_by <- intersect(c("Parameter", "Effects", "Component"), colnames(temp))
   temp$.roworder <- 1:nrow(temp)
   out <- merge(x = temp, y = cleaned_parameters, by = merge_by, all.x = TRUE)
+  out <- out[!is.na(out$Effects) & !is.na(out$Component), ]
   attr(out, "Cleaned_Parameter") <- out$Cleaned_Parameter[order(out$.roworder)]
   .remove_column(out[order(out$.roworder), ], c("Group", "Cleaned_Parameter", "Response", "Function", ".roworder"))
 }
