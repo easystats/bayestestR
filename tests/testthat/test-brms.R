1207e787990032e0e8410afd0f5c64028631879f
Daniel <mail@danielluedecke.de>
2020-07-26 11:51:07 +0200

    add more tests

--- tests/testthat/test-brms.R
+++ tests/testthat/test-brms.R
@@ -18,5 +18,51 @@ if (require("brms", quietly = TRUE)) {
     testthat::expect_equal(colnames(hdi(model)), c("Parameter", "CI", "CI_low", "CI_high", "Effects", "Component"))
     testthat::expect_equal(colnames(hdi(model, effects = "all")), c("Parameter", "CI", "CI_low", "CI_high", "Effects", "Component"))
     # testthat::expect_equal(nrow(equivalence_test(model)), 2)
+
+    out <- describe_posterior(model, effects = "all", components = "all", centrality = "mean")
+    s <- summary(model)
+    testthat::expect_equal(s$fixed[, 1, drop = TRUE], out$Mean[1:2], check.attributes = FALSE, tolerance = 1e-3)
+    testthat::expect_equal(s$fixed[, 5, drop = TRUE], out$Rhat[1:2], check.attributes = FALSE, tolerance = 1e-1)
+    testthat::expect_equal(s$random$cyl[, 1, drop = TRUE], out$Mean[12], check.attributes = FALSE, tolerance = 1e-3)
+    testthat::expect_equal(s$random$gear[, 1, drop = TRUE], out$Mean[13:15], check.attributes = FALSE, tolerance = 1e-3)
+  })
+
+  test_that("brms", {
+    # testthat::skip_on_travis()
+    testthat::skip_on_cran()
+
+    set.seed(333)
+    model <- insight::download_model("brms_1")
+
+    out <- describe_posterior(model, effects = "all", components = "all", centrality = "mean")
+    s <- summary(model)
+    testthat::expect_equal(s$fixed[, 1, drop = TRUE], out$Mean[1:3], check.attributes = FALSE, tolerance = 1e-3)
+    testthat::expect_equal(s$fixed[, 5, drop = TRUE], out$Rhat[1:3], check.attributes = FALSE, tolerance = 1e-1)
+  })
+
+  test_that("brms", {
+    # testthat::skip_on_travis()
+    testthat::skip_on_cran()
+
+    set.seed(333)
+    model <- insight::download_model("brms_mv_2")
+
+    out <- describe_posterior(model, effects = "all", components = "all", centrality = "mean", test = NULL)
+    s <- summary(model)
+    testthat::expect_equal(s$fixed[, 1, drop = TRUE], out$Mean[c(1, 11, 2:5, 12:14)], check.attributes = FALSE, tolerance = 1e-3)
+    testthat::expect_equal(s$fixed[, 5, drop = TRUE], out$Rhat[c(1, 11, 2:5, 12:14)], check.attributes = FALSE, tolerance = 1e-1)
+  })
+
+  test_that("brms", {
+    # testthat::skip_on_travis()
+    testthat::skip_on_cran()
+
+    set.seed(333)
+    model <- insight::download_model("brms_2")
+
+    out <- describe_posterior(model, effects = "all", components = "all", centrality = "mean", test = NULL)
+    s <- summary(model)
+    testthat::expect_equal(s$fixed[, 1, drop = TRUE], out$Mean, check.attributes = FALSE, tolerance = 1e-3)
+    testthat::expect_equal(s$fixed[, 5, drop = TRUE], out$Rhat, check.attributes = FALSE, tolerance = 1e-1)
   })
 }
