e638300875f2bec12a633335b9c657cfa70a6261
DominiqueMakowski <dom.mak19@gmail.com>
2020-10-26 16:13:37 +0800

    add startup message
    #250

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   message <- "Note: The default CI width might change in future versions (see https://github.com/easystats/bayestestR/issues/250).
-To prevent any issues, please set it explicitly when calling bayestestR functions, via the 'ci' argument."
+To prevent any issues, please set it explicitly when using bayestestR functions, via the 'ci' argument."
   if (format(Sys.time(), "%m%d") == "0504") {
     message <- paste0(message, "\n\nMay the fourth be with you!")
   }
