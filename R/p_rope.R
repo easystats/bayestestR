fb622fc2a10824ede0feda1b2774700bf0c1f816
DominiqueMakowski <dom.mak19@gmail.com>
2020-10-28 10:12:51 +0800

    fix docs for p_rope
    close #349

--- R/p_rope.R
+++ R/p_rope.R
@@ -1,6 +1,6 @@
-#' Probability of not being in ROPE
+#' Probability of being in the ROPE
 #'
-#' Compute the proportion of the posterior distribution that doesn't lie within a region of practical equivalence (ROPE). It is equivalent to running \code{rope(..., ci = 1)}.
+#' Compute the proportion of the whole posterior distribution that doesn't lie within a region of practical equivalence (ROPE). It is equivalent to running \code{rope(..., ci = 1)}.
 #'
 #' @inheritParams rope
 #'
