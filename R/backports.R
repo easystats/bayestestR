607e8bd9cbff2fcd95cdd241854c1c9441c41cec
Daniel <mail@danielluedecke.de>
2021-01-17 14:20:35 +0100

    Update backports.R

--- R/backports.R
+++ R/backports.R
@@ -1,15 +1,3 @@
-.str2lang <- function(s) {
-  if (getRversion() < "3.6.0") {
-    stopifnot(length(s) == 1L)
-    ex <- parse(text = s, keep.source = FALSE)
-    stopifnot(length(ex) == 1L)
-    ex[[1L]]
-  } else {
-    str2lang(s)
-  }
-}
-
-
 isTRUE <- function(x) {
   is.logical(x) && length(x) == 1L && !is.na(x) && x
 }
