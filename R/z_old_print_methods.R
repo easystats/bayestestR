52140c713f81a088c9ed1ba22de3dfc69c47484e
Daniel <mail@danielluedecke.de>
2021-03-14 14:09:03 +0100

    cleanup

--- R/z_old_print_methods.R
+++ R/z_old_print_methods.R
@@ -131,3 +131,18 @@
 #     }
 #   }
 # }
+
+
+
+# print.point_estimate <- function(x, digits = 2, ...) {
+#   orig_x <- x
+#   if ("data_plot" %in% class(x)) {
+#     print(as.data.frame(x))
+#   } else if ("data.frame" %in% class(x)) {
+#     insight::print_color("# Point Estimates\n\n", "blue")
+#     print_data_frame(x, digits = digits)
+#   } else {
+#     print(unclass(x))
+#   }
+#   invisible(orig_x)
+# }
