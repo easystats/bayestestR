43e4e746fff16d6194e6dbe7239d7d45e9d8708d
Dominique Makowski <dom.mak19@gmail.com>
2021-03-29 19:45:18 +0800

    hotfix

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   if (format(Sys.time(), "%m%d") == "0504") {
-    message <- paste0(message, "\n\nMay the fourth be with you!")
+    packageStartupMessage("May the fourth be with you!")
   }
-  packageStartupMessage(message)
 }
