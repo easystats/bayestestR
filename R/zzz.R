b4fdf51c065cb774b2cf5e20b0ade8d927dfc4c0
Dominique Makowski <dom.mak19@gmail.com>
2021-03-29 19:44:08 +0800

    finally, peace :sleeping: :sunrise:

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-  message <- "Note: The default CI width just changed :O To go back to the previous behaviour, set it explicitly as 'ci = 0.89' (see https://github.com/easystats/bayestestR/discussions/250)"
   if (format(Sys.time(), "%m%d") == "0504") {
     message <- paste0(message, "\n\nMay the fourth be with you!")
   }
