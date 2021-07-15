f730170f10577ec048fb3b7cae43f9b46298c0ca
Daniel <mail@danielluedecke.de>
2021-07-16 00:18:06 +0200

    use namespace

--- R/diagnostic_posterior.R
+++ R/diagnostic_posterior.R
@@ -288,7 +288,7 @@ diagnostic_posterior.blavaan <- function(posteriors, diagnostic = "all", ...) {
 
     Rhat <- blavaan::blavInspect(posteriors, what = "psrf")
     Rhat <- data.frame(
-      Parameter = colnames(get_parameters(posteriors)),
+      Parameter = colnames(insight::get_parameters(posteriors)),
       Rhat = Rhat
     )
     out <- merge(out, Rhat, by = "Parameter", all = TRUE)
