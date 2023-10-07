cadb4d6ab44ea90f23ab0cad10b5d608b40b1866
Daniel <mail@danielluedecke.de>
2023-10-07 16:53:57 +0200

    update test

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -5,6 +5,6 @@ test_that("print.describe_posterior", {
   skip_if_not_or_load_if_installed("httr")
 
   m <- insight::download_model("brms_zi_3")
-  expect_snapshot(describe_posterior(m), variant = "windows")
-  expect_snapshot(describe_posterior(m, effects = "all", component = "all"), variant = "windows")
+  expect_snapshot(describe_posterior(m, verbose = FALSE), variant = "windows")
+  expect_snapshot(describe_posterior(m, effects = "all", component = "all", verbose = FALSE), variant = "windows")
 })
