8af222c1880c8834ddf7d3dc4f7154c0d02254ba
Daniel <mail@danielluedecke.de>
2025-08-24 19:44:45 +0200

    minor

--- R/utils.R
+++ R/utils.R
@@ -337,7 +337,7 @@
 .get_marginaleffects_draws <- function(object) {
   # errors and checks are handled by marginaleffects
   insight::check_if_installed("marginaleffects", minimum_version = "0.24.0")
-  data.frame(marginaleffects::get_draws(object, shape = "DxP"))
+  as.data.frame(marginaleffects::get_draws(object, shape = "DxP"))
 }
 
 
