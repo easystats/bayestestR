c522a7cde6d6b79211480c35a4dc87548e8b4fae
Daniel <mail@danielluedecke.de>
2025-08-26 17:28:34 +0200

    fix

--- R/display.R
+++ R/display.R
@@ -102,6 +102,6 @@ display.bayesfactor_inclusion <- display.describe_posterior
 }
 
 .display_default_format <- function(format) {
-  format <- getOption("easystats_display_format", "markdown")
+  format <- getOption("easystats_display_format", format)
   insight::validate_argument(format, c("markdown", "html", "md", "tt"))
 }
