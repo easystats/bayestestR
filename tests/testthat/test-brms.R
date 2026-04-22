67e89566b17d52f6b7e6d2ed6c15d49c2ec6cd9a
Daniel <mail@danielluedecke.de>
2026-04-22 16:44:29 +0200

    fix tests

--- tests/testthat/test-brms.R
+++ tests/testthat/test-brms.R
@@ -1,4 +1,4 @@
-test_that("brms", {
+test_that("brms mixed", {
   skip_on_cran()
   skip_if_not_installed("curl")
   skip_if_offline()
@@ -17,26 +17,55 @@ test_that("brms", {
   expect_s3_class(p_map(model), "data.frame")
   expect_s3_class(p_direction(model), "data.frame")
 
-  expect_named(hdi(model), c("Parameter", "CI", "CI_low", "CI_high", "Effects", "Component"))
-  expect_named(hdi(model, effects = "all"), c("Parameter", "CI", "CI_low", "CI_high", "Effects", "Component"))
+  expect_named(
+    hdi(model),
+    c("Parameter", "CI", "CI_low", "CI_high", "Effects", "Component")
+  )
+  expect_named(
+    hdi(model, effects = "all"),
+    c("Parameter", "CI", "CI_low", "CI_high", "Effects", "Component")
+  )
   expect_identical(nrow(equivalence_test(model)), 2L)
 
-  out <- describe_posterior(model, effects = "all", component = "all", centrality = "mean")
+  out <- describe_posterior(
+    model,
+    effects = "all",
+    component = "all",
+    centrality = "mean"
+  )
   suppressWarnings({
     s <- summary(model)
   })
-  expect_identical(colnames(out), c(
-    "Parameter", "Effects", "Component", "Mean", "CI", "CI_low", "CI_high",
-    "pd", "ROPE_CI", "ROPE_low", "ROPE_high", "ROPE_Percentage",
-    "Rhat", "ESS"
-  ))
+  expect_identical(
+    colnames(out),
+    c(
+      "Parameter",
+      "Effects",
+      "Component",
+      "Mean",
+      "CI",
+      "CI_low",
+      "CI_high",
+      "pd",
+      "ROPE_CI",
+      "ROPE_low",
+      "ROPE_high",
+      "ROPE_Percentage",
+      "Rhat",
+      "ESS_tail"
+    )
+  )
   expect_equal(as.vector(s$fixed[, 1, drop = TRUE]), out$Mean[1:2], tolerance = 1e-3)
   expect_equal(as.vector(s$fixed[, 5, drop = TRUE]), out$Rhat[1:2], tolerance = 1e-1)
   expect_equal(as.vector(s$random$cyl[, 1, drop = TRUE]), out$Mean[3], tolerance = 1e-3)
-  expect_equal(as.vector(s$random$gear[, 1, drop = TRUE]), out$Mean[4:6], tolerance = 1e-3)
+  expect_equal(
+    as.vector(s$random$gear[, 1, drop = TRUE]),
+    out$Mean[4:6],
+    tolerance = 1e-3
+  )
 })
 
-test_that("brms", {
+test_that("brms standard", {
   skip_on_cran()
   skip_if_not_installed("curl")
   skip_if_offline()
@@ -47,17 +76,36 @@ test_that("brms", {
   model <- insight::download_model("brms_1")
   skip_if(is.null(model))
 
-  out <- describe_posterior(model, effects = "all", component = "all", centrality = "mean")
+  out <- describe_posterior(
+    model,
+    effects = "all",
+    component = "all",
+    centrality = "mean"
+  )
   s <- summary(model)
-  expect_identical(colnames(out), c(
-    "Parameter", "Component", "Mean", "CI", "CI_low", "CI_high", "pd", "ROPE_CI",
-    "ROPE_low", "ROPE_high", "ROPE_Percentage", "Rhat", "ESS"
-  ))
+  expect_identical(
+    colnames(out),
+    c(
+      "Parameter",
+      "Component",
+      "Mean",
+      "CI",
+      "CI_low",
+      "CI_high",
+      "pd",
+      "ROPE_CI",
+      "ROPE_low",
+      "ROPE_high",
+      "ROPE_Percentage",
+      "Rhat",
+      "ESS_tail"
+    )
+  )
   expect_equal(as.vector(s$fixed[, 1, drop = TRUE]), out$Mean[1:3], tolerance = 1e-3)
   expect_equal(as.vector(s$fixed[, 5, drop = TRUE]), out$Rhat[1:3], tolerance = 1e-1)
 })
 
-test_that("brms", {
+test_that("brms multivariate", {
   skip_on_cran()
   skip_if_not_installed("curl")
   skip_if_offline()
@@ -68,12 +116,28 @@ test_that("brms", {
   model <- insight::download_model("brms_mv_2")
   skip_if(is.null(model))
 
-  out <- describe_posterior(model, effects = "all", component = "all", centrality = "mean", test = NULL)
+  out <- describe_posterior(
+    model,
+    effects = "all",
+    component = "all",
+    centrality = "mean",
+    test = NULL
+  )
   s <- suppressWarnings(summary(model))
-  expect_identical(colnames(out), c(
-    "Parameter", "Effects", "Component", "Mean", "CI", "CI_low", "CI_high",
-    "Rhat", "ESS"
-  ))
+  expect_identical(
+    colnames(out),
+    c(
+      "Parameter",
+      "Effects",
+      "Component",
+      "Mean",
+      "CI",
+      "CI_low",
+      "CI_high",
+      "Rhat",
+      "ESS_tail"
+    )
+  )
 
   known <- s$fixed
   unknown <- out[out$Effects == "fixed" & out$Component == "conditional", ]
@@ -83,7 +147,7 @@ test_that("brms", {
   expect_equal(unknown$Rhat, known$Rhat, tolerance = 1e-2, ignore_attr = TRUE)
 })
 
-test_that("brms", {
+test_that("brms standard, 2", {
   skip_on_cran()
   skip_if_not_installed("curl")
   skip_if_offline()
@@ -94,7 +158,13 @@ test_that("brms", {
   model <- insight::download_model("brms_2")
   skip_if(is.null(model))
 
-  out <- describe_posterior(model, effects = "all", component = "all", centrality = "mean", test = NULL)
+  out <- describe_posterior(
+    model,
+    effects = "all",
+    component = "all",
+    centrality = "mean",
+    test = NULL
+  )
   s <- summary(model)
   expect_equal(as.vector(s$fixed[, 1, drop = TRUE]), out$Mean, tolerance = 1e-3)
   expect_equal(as.vector(s$fixed[, 5, drop = TRUE]), out$Rhat, tolerance = 1e-1)
